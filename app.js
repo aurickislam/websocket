4a18e9d9678f79c576ac1532d1a7b7c1b9350b1b
Md. Imadul Islam <aurick88@gmail.com>
2020-08-01 15:53:34 +0600

    Dokcerfile added

--- app.js
+++ app.js
@@ -3,32 +3,43 @@ const http = require('http'),
 	WebSocket = require('ws'),
 	colors = require('colors');
 
+if (process.env.COLOR !== 'true')
+	colors.disable();
+
 const app = express(),
 	server = http.createServer(app);
 
-const wss = new WebSocket.Server({server, path:"/broadcast"});
-wss.on('connection', (ws, req) => {
-	ws.on('message', message => {
+const wsServer = new WebSocket.Server({server, path:"/broadcast"});
+wsServer.on('connection', (wsClient, req) => {
+	wsClient.on('message', message => {
 		console.log(`received: ${message}`);
-		ws.send(`Hello, you sent -> ${message}`);
+		// wsClient.send(message);
+
+		console.log("@wsServer.clients", wsServer.clients.size);
+
+		wsServer.clients.forEach(client => {
+			if (client != wsClient)
+				client.send(message);
+		});
 	});
 
-	// ws.send('Hi there, I am a WebSocket server');
 	// ws.send(Uint8Array.from(Buffer.from(JSON.stringify({a: 'a'}))).buffer);
-	ws.send(JSON.stringify({a: 'a'}));
+	// wsClient.send(JSON.stringify({message: 'Connected'}));
+	wsClient.send('Websocket connected successfully');
 });
 
 
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/broadcast', (req, res) => {
+app.post('/broadcast', (req, res) => {
+	console.log("@req", req.body)
 
 	console.log("@request", req.query.message);
 
-	wss.clients.forEach(client => {
-		// if (client != ws)
-		client.send(`Hello, broadcast message -> ${req.query.message}`);
+	wsServer.clients.forEach(client => {
+		// if (waClient != ws)
+		client.send(`${req.query.message}`);
 	});
 
 	res.send({
--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,33 +1,10 @@
-let validationRules;
-
 $(function () {
-	$('.broadcast').click(function (e) {
-		$.ajax({
-			type: 'GET',
-			url: "/broadcast?message=" + $('#input-box').val(),
-			cache: false,
-			contentType: 'application/json',
-			processData: false,
-			timeout: 10000,
-			success: function (response, status, xhr) {
-				console.log("@response", response);
-				console.log("@status", status);
-				console.log("@xhr", xhr);
-			},
-			error: function () {
-				console.log("failed");
-				$.notify("Failed to save", "error");
-			}
-		});
-	});
-
-
-	const socket = new WebSocket("ws://192.168.88.20:8999/broadcast");
+	const socket = new WebSocket("ws://192.168.88.2:8999/broadcast");
 
 	socket.onopen = function(e) {
 		console.log("[open] Connection established");
 		console.log("Sending to server");
-		socket.send("My name is John");
+		// socket.send("My name is Aurick");
 	};
 
 	socket.onmessage = function(event) {
@@ -49,4 +26,29 @@ $(function () {
 		console.log(`[error] ${error.message}`);
 	};
 
+	$('.sendWS').click(function (e) {
+		socket.send($('#input-box').val());
+	});
+
+	$('.send').click(function (e) {
+		const message = $('#input-box').val();
+		$.ajax({
+			type: 'POST',
+			url: `/broadcast?message=${message}`,
+			data: JSON.stringify({message: message}),
+			cache: false,
+			contentType: 'application/json',
+			processData: false,
+			timeout: 10000,
+			success: function (response, status, xhr) {
+				console.log("@response", response);
+				console.log("@status", status);
+				console.log("@xhr", xhr);
+			},
+			error: function () {
+				console.log("failed");
+				$.notify("Failed to save", "error");
+			}
+		});
+	});
 });
