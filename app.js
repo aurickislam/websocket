b029a93b8353c6fb6af6f88c73369bd3e446b59b
Md. Imadul Islam <aurick88@gmail.com>
2020-09-26 04:35:32 +0600

    Docker image version changes

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const http = require('http'),
-	express = require('express'),
-	WebSocket = require('ws'),
-	colors = require('colors');
+    express = require('express'),
+    WebSocket = require('ws'),
+    colors = require('colors');
 
 if (process.env.COLOR !== 'true')
-	colors.disable();
+    colors.disable();
 
 const app = express(),
-	server = http.createServer(app);
+    server = http.createServer(app);
 
-const wsServer = new WebSocket.Server({server, path:"/broadcast"});
+const wsServer = new WebSocket.Server({server, path: "/broadcast"});
 wsServer.on('connection', (wsClient, req) => {
-	wsClient.on('message', message => {
-		console.log(`received: ${message}`);
-		// wsClient.send(message);
+    wsClient.on('message', message => {
+        console.log(`received: ${message}`);
+        // wsClient.send(message);
 
-		console.log("@wsServer.clients", wsServer.clients.size);
+        console.log("@wsServer.clients", wsServer.clients.size);
 
-		wsServer.clients.forEach(client => {
-			if (client != wsClient)
-				client.send(message);
-		});
-	});
+        wsServer.clients.forEach(client => {
+            if (client != wsClient)
+                client.send(message);
+        });
+    });
 
-	// ws.send(Uint8Array.from(Buffer.from(JSON.stringify({a: 'a'}))).buffer);
-	// wsClient.send(JSON.stringify({message: 'Connected'}));
-	wsClient.send('Websocket connected successfully');
+    // ws.send(Uint8Array.from(Buffer.from(JSON.stringify({a: 'a'}))).buffer);
+    // wsClient.send(JSON.stringify({message: 'Connected'}));
+    wsClient.send('Websocket connected successfully');
 });
 
 
@@ -33,23 +33,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/broadcast', (req, res) => {
-	console.log("@req", req.body)
+    console.log("@req", req.body)
 
-	console.log("@request", req.query.message);
+    console.log("@request", req.query.message);
 
-	wsServer.clients.forEach(client => {
-		// if (waClient != ws)
-		client.send(`${req.query.message}`);
-	});
+    wsServer.clients.forEach(client => {
+        // if (waClient != ws)
+        client.send(`${req.query.message}`);
+    });
 
-	res.send({
-		status: "OK"
-	});
+    res.send({
+        status: "OK"
+    });
 });
 
 //start our server
-server.listen(process.env.PORT || 8999, () => {
-	console.log("\n===========================".rainbow);
-	console.log(`Server started on port ${server.address().port}`.cyan);
-	console.log("===========================\n".rainbow);
+server.listen(8999, () => {
+    console.log("\n===========================".rainbow);
+    console.log(`Server started on port ${server.address().port}`.cyan);
+    console.log("===========================\n".rainbow);
 });
